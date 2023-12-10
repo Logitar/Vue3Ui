ba65bdf9944fe787c6f37f5047557a6d3339fb8c
Francis Pion <francispion@hotmail.com>
2023-12-10 00:09:12 -0500

    Removed TODOs. (#20)

--- src/components/TarInput.vue
+++ src/components/TarInput.vue
@@ -45,9 +45,6 @@ const inputRef = ref<HTMLInputElement>();
 
 const classes = computed<string[]>(() => {
   const classes = ["form-control"];
-  // if (!props.noState && (meta.dirty || meta.touched)) {
-  //   classes.push(meta.valid ? "is-valid" : "is-invalid");
-  // } // TODO(fpion): implement
   return classes;
 });
 
@@ -55,40 +52,19 @@ function focus(): void {
   inputRef.value?.focus();
 }
 defineExpose({ focus });
-
-// max/min: date, month, week, time, datetime-local, number, range
-// maxlength/minlength: text, search, url, tel, email, password
-// name
-// pattern
-// required
-// size: text, search, url, tel, email, password
-// step: date, month, week, time, datetime-local, number, range
-// value: all except image
 </script>
 
 <template>
   <div class="mb-3">
-    <label v-if="label" :for="id" class="form-label">
-      <!-- TODO(fpion): inline? nolabel? -->
-      <!-- <template v-if="required"><span class="text-danger">*</span></template> {{ t(label) }} -->
-      {{ label }}
-    </label>
+    <label v-if="label" :for="id" class="form-label">{{ label }}</label>
     <slot name="before"></slot>
     <div class="input-group">
       <slot name="prepend"></slot>
       <slot>
-        <!-- <input
-        :max="inputMax"
-        :min="inputMin"
-        :step="type === 'number' ? step : undefined"
-        :value="value"
-        v-on="validationListeners"
-      /> -->
         <input :class="classes" :disabled="disabled" :id="id" :name="name" :placeholder="placeholder" :readonly="readonly" ref="inputRef" :type="type" />
       </slot>
       <slot name="append"></slot>
     </div>
     <slot name="after"></slot>
-    <!-- <div v-if="errorMessage" class="invalid-feedback">{{ errorMessage }}</div> -->
   </div>
 </template>
