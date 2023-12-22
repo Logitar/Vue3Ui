50fb8ae23338a5d6cb4a6b2a0724197ba8b8afd8
Francis Pion <francispion@hotmail.com>
2023-12-21 22:01:13 -0500

    Exports.

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import TarCheckbox from "./components/TarCheckbox.vue";
+import type { CheckboxOptions } from "./types/TarCheckbox";
+
+export type { CheckboxOptions };
+export { TarCheckbox };
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,16 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from "vue";
+
+import TarCheckbox from "@/components/TarCheckbox.vue";
+
+const remember = ref<boolean>(false);
+</script>
 
 <template>
   <main class="container">
     <h1>Home</h1>
+    <p>Remember Me: {{ remember }}</p>
+    <TarCheckbox label="Remember Me" switch v-model="remember" />
+    <TarCheckbox disabled label="Disabled Checkbox" />
   </main>
 </template>
