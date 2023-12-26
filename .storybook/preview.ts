45bbb323c083063010b112ddb27de7b3361e42da
Francis Pion <francispion@hotmail.com>
2023-12-25 23:43:54 -0500

    Installed Storybook and added Checkbox stories.

---
+++ .storybook/preview.ts
@@ -0,0 +1,18 @@
+import type { Preview } from "@storybook/vue3";
+
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const preview: Preview = {
+  parameters: {
+    actions: { argTypesRegex: "^on[A-Z].*" },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+};
+
+export default preview;
---
+++ src/components/TarCheckbox.stories.ts
@@ -0,0 +1,90 @@
+import type { Meta, StoryObj } from "@storybook/vue3";
+
+import TarCheckbox from "./TarCheckbox.vue";
+
+const meta = {
+  title: "Components/TarCheckbox",
+  component: TarCheckbox,
+  tags: ["autodocs"],
+  args: {
+    ariaLabel: "",
+    disabled: false,
+    id: "completed",
+    inline: false,
+    label: "Completed",
+    modelValue: false,
+    name: "",
+    required: false,
+    reverse: false,
+    role: "",
+    switch: false,
+    value: "",
+  },
+} satisfies Meta<typeof TarCheckbox>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {};
+
+export const CheckboxVsSwitch: Story = {
+  name: "Checkbox vs. Switch",
+  render: (args) => ({
+    components: { TarCheckbox },
+    setup() {
+      return { args };
+    },
+    template: `
+      <TarCheckbox v-bind="args" id="checkbox" />
+      <TarCheckbox v-bind="args" id="switch" switch />
+    `,
+  }),
+};
+
+export const Disabled: Story = {
+  args: {
+    disabled: true,
+  },
+  render: (args) => ({
+    components: { TarCheckbox },
+    setup() {
+      return { args };
+    },
+    template: `
+      <TarCheckbox v-bind="args" id="disabled-checkbox" />
+      <TarCheckbox v-bind="args" id="disabled-switch" switch />
+    `,
+  }),
+};
+
+export const Inline: Story = {
+  args: {
+    inline: true,
+  },
+  render: (args) => ({
+    components: { TarCheckbox },
+    setup() {
+      return { args };
+    },
+    template: `
+      <TarCheckbox v-bind="args" id="inline-checkbox" />
+      <TarCheckbox v-bind="args" id="inline-switch" switch />
+    `,
+  }),
+};
+
+export const Reverse: Story = {
+  args: {
+    reverse: true,
+  },
+  render: (args) => ({
+    components: { TarCheckbox },
+    setup() {
+      return { args };
+    },
+    template: `
+      <TarCheckbox v-bind="args" id="reverse-checkbox" />
+      <TarCheckbox v-bind="args" id="reverse-switch" switch />
+    `,
+  }),
+};
