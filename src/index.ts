c7aabc5288612109ab7a156d53e95d02192e1a7c
Francis Pion <francispion@hotmail.com>
2023-12-25 23:32:23 -0500

    Implemented a Spinner component.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import * as parsingUtils from "./helpers/parsingUtils";
 import TarCheckbox from "./components/TarCheckbox.vue";
+import TarSpinner from "./components/TarSpinner.vue";
 
 export * from "./types/TarCheckbox";
-export { TarCheckbox, parsingUtils };
+export * from "./types/TarSpinner";
+export { TarCheckbox, TarSpinner, parsingUtils };
