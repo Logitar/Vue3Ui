494921751ff69e052437f01209576b9ee6396a21
Francis Pion <francispion@hotmail.com>
2024-01-01 20:38:33 -0500

    Implemented an Image component.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -16,3 +16,4 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 - Implemented a Spinner component.
 - Implemented a Tab component.
 - Implemented an Alert component.
+- Implemented an Image component.
--- src/helpers/parsingUtils.ts
+++ src/helpers/parsingUtils.ts
@@ -11,3 +11,15 @@ export function parseBoolean(value: boolean | string | undefined): boolean | und
     }
   }
 }
+
+export function parseNumber(value: number | string | undefined): number | undefined {
+  if (typeof value === "number") {
+    return value;
+  } else if (typeof value === "string") {
+    const number = Number(value.trim());
+    if (!isNaN(number)) {
+      return number;
+    }
+  }
+  return undefined;
+}
