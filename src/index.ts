0825df553d7b08576d14e04e0d33df15f42a726d
Francis Pion <francispion@hotmail.com>
2024-01-01 19:19:30 -0500

    Fixed exports.

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,12 @@ import TarAlert from "./components/TarAlert.vue";
 import TarButton from "./components/TarButton.vue";
 import TarCheckbox from "./components/TarCheckbox.vue";
 import TarSpinner from "./components/TarSpinner.vue";
+import TarTab from "./components/TarTab.vue";
+import TarTabs from "./components/TarTabs.vue";
 
 export * from "./types/TarAlert";
 export * from "./types/TarButton";
 export * from "./types/TarCheckbox";
 export * from "./types/TarSpinner";
-export { TarAlert, TarButton, TarCheckbox, TarSpinner, parsingUtils };
+export * from "./types/TarTabs";
+export { TarAlert, TarButton, TarCheckbox, TarSpinner, TarTab, TarTabs, parsingUtils };
