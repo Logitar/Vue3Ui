596e6624560a91c27e91d070fd5cceeace116cc5
Francis Pion <francispion@hotmail.com>
2024-01-02 14:24:59 -0500

    Exporting modal component.

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,7 @@ import TarCarousel from "./components/TarCarousel.vue";
 import TarCheckbox from "./components/TarCheckbox.vue";
 import TarImage from "./components/TarImage.vue";
 import TarInput from "./components/TarInput.vue";
+import TarModal from "./components/TarModal.vue";
 import TarProgress from "./components/TarProgress.vue";
 import TarSelect from "./components/TarSelect.vue";
 import TarSpinner from "./components/TarSpinner.vue";
@@ -26,6 +27,7 @@ export * from "./types/TarCarousel";
 export * from "./types/TarCheckbox";
 export * from "./types/TarImage";
 export * from "./types/TarInput";
+export * from "./types/TarModal";
 export * from "./types/TarProgress";
 export * from "./types/TarSelect";
 export * from "./types/TarSpinner";
@@ -42,6 +44,7 @@ export {
   TarCheckbox,
   TarImage,
   TarInput,
+  TarModal,
   TarProgress,
   TarSelect,
   TarSpinner,
