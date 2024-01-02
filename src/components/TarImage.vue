66783251d757280a300d0063e3a70aaf736ce0fb
Francis Pion <francispion@hotmail.com>
2024-01-01 20:39:27 -0500

    Implemented an Image component. (#81)

---
+++ src/components/TarImage.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { computed } from "vue";
+
+import type { ImageOptions } from "@/types/TarImage";
+import { parseBoolean, parseNumber } from "@/helpers/parsingUtils";
+
+const props = defineProps<ImageOptions>();
+
+const classes = computed<string[]>(() => {
+  const classes: string[] = [];
+  if (parseBoolean(props.circle)) {
+    classes.push("rounded-circle");
+  } else if (parseBoolean(props.rounded)) {
+    classes.push("rounded");
+  }
+  if (parseBoolean(props.fluid)) {
+    classes.push("img-fluid");
+  }
+  if (parseBoolean(props.thumbnail)) {
+    classes.push("img-thumbnail");
+  }
+  return classes;
+});
+</script>
+
+<template>
+  <img :alt="alt" :class="classes" :height="parseNumber(height)" :src="src" :width="parseNumber(width)" />
+</template>
--- src/helpers/__tests__/parsingUtils.spec.ts
+++ src/helpers/__tests__/parsingUtils.spec.ts
@@ -1,6 +1,6 @@
 import { describe, expect, it } from "vitest";
 
-import { parseBoolean } from "../parsingUtils";
+import { parseBoolean, parseNumber } from "../parsingUtils";
 
 describe("parsingUtils.parseBoolean", () => {
   it.concurrent("should return the same value when it is a boolean", () => {
@@ -25,5 +25,37 @@ describe("parsingUtils.parseBoolean", () => {
     expect(parseBoolean(undefined)).toBeUndefined();
     expect(parseBoolean(null as any)).toBeUndefined();
     expect(parseBoolean(123 as any)).toBeUndefined();
+  });
+});
+
+describe("parsingUtils.parseNumber", () => {
+  it.concurrent("should not parse a non-numberish string", () => {
+    expect(parseNumber("test")).toBeUndefined();
+    expect(parseNumber("Hello World!")).toBeUndefined();
+  });
+
+  it.concurrent("should not parse any other type", () => {
+    expect(parseNumber(false as any)).toBeUndefined();
+    expect(parseNumber(true as any)).toBeUndefined();
+    expect(parseNumber(new Date() as any)).toBeUndefined();
+    expect(parseNumber(undefined as any)).toBeUndefined();
+    expect(parseNumber(null as any)).toBeUndefined();
+    expect(parseNumber({ number: 123 } as any)).toBeUndefined();
+  });
+
+  it.concurrent("should parse a number", () => {
+    expect(parseNumber(-1)).toBe(-1);
+    expect(parseNumber(0)).toBe(0);
+    expect(parseNumber(420)).toBe(420);
+  });
+
+  it.concurrent("should parse a numberish string", () => {
+    expect(parseNumber("-10")).toBe(-10);
+    expect(parseNumber(" -2 ")).toBe(-2);
+    expect(parseNumber("")).toBe(0);
+    expect(parseNumber("   ")).toBe(0);
+    expect(parseNumber("0")).toBe(0);
+    expect(parseNumber("247")).toBe(247);
+    expect(parseNumber("  9  ")).toBe(9);
   });
 });
