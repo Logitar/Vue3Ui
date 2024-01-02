215f4bde13de90cf69c58620d2e5082db3a7c028
Francis Pion <francispion@hotmail.com>
2024-01-02 14:33:13 -0500

    Implemented a Pagination component. (#96)

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,7 @@ import TarCheckbox from "./components/TarCheckbox.vue";
 import TarImage from "./components/TarImage.vue";
 import TarInput from "./components/TarInput.vue";
 import TarModal from "./components/TarModal.vue";
+import TarPagination from "./components/TarPagination.vue";
 import TarProgress from "./components/TarProgress.vue";
 import TarSelect from "./components/TarSelect.vue";
 import TarSpinner from "./components/TarSpinner.vue";
@@ -28,6 +29,7 @@ export * from "./types/TarCheckbox";
 export * from "./types/TarImage";
 export * from "./types/TarInput";
 export * from "./types/TarModal";
+export * from "./types/TarPagination";
 export * from "./types/TarProgress";
 export * from "./types/TarSelect";
 export * from "./types/TarSpinner";
@@ -45,6 +47,7 @@ export {
   TarImage,
   TarInput,
   TarModal,
+  TarPagination,
   TarProgress,
   TarSelect,
   TarSpinner,
