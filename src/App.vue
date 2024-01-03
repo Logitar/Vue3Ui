b8ca8ef650597d092bfa14ce924b3f3211641497
Francis Pion <francispion@hotmail.com>
2024-01-02 22:15:38 -0500

    Implemented a Demo toast with error handling. (#104)
    * Implemented a Demo toast with error handling.
    
    * Typing.

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,35 @@
 <script setup lang="ts">
+import { ref } from "vue";
+
 import TarButton from "@/components/TarButton.vue";
+import TarToaster from "./components/TarToaster.vue";
+
+const toaster = ref<InstanceType<typeof TarToaster>>();
 
 function onClick(): void {
-  alert("Hello World!");
+  if (toaster.value) {
+    toaster.value.toast({
+      duration: 15 * 1000,
+      fade: true,
+      text: "Hello World!",
+      title: "Message",
+      variant: "info",
+    });
+  }
+}
+
+function onError(): void {
+  throw new Error("Oops! Something happened...");
 }
 </script>
 
 <template>
   <main class="container">
     <h1>Home</h1>
-    <TarButton text="Click me!" @click="onClick" />
+    <div class="mb-3">
+      <TarButton class="me-2" text="Message" @click="onClick" />
+      <TarButton text="Error" variant="danger" @click="onError" />
+    </div>
+    <TarToaster ref="toaster" />
   </main>
 </template>
