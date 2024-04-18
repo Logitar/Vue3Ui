db115fc32cf0a024666f7102ae9c4c78dee41315
Francis Pion <francispion@hotmail.com>
2024-04-17 20:57:19 -0400

    Upgraded NuGet packages.

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,7 +3,7 @@ import type { StorybookConfig } from "@storybook/vue3-vite";
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   staticDirs: ["../public"],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions"],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions", "@chromatic-com/storybook"],
   framework: {
     name: "@storybook/vue3-vite",
     options: {},
--- CHANGELOG.md
+++ CHANGELOG.md
@@ -19,6 +19,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 - Refactored `TarInput` type property handling.
 - Replaced `...` by `…`.
 - Replaced `yarn` by NPM.
+- Upgraded NuGet packages.
 
 ### Fixed
 
